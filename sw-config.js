cc400852db570e00c58cf61bc3775e35707ab2c9
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-06-08 21:49:42 +0700

    chore: undo changes

--- sw-config.js
+++ sw-config.js
@@ -1,17 +1,20 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Your preprocessors are unchanged and correct.
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 
   kit: {
-    // 2. The adapter is now set to adapter-node with no special options.
-    adapter: adapter(),
-
-    // Your custom file paths are unchanged and correct.
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      fallback: "index.html",
+    }),
     files: {
       routes: "src/routes-sw",
     },
